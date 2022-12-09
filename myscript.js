74aeb5903beded2a916088f3b21d1366a5dc8128
Simply one <selvam.s3403@gmail.com>
2022-12-09 16:31:43 +0530

    11

--- myscript.js
+++ myscript.js
@@ -3,3 +3,17 @@
 AOS.init();
 
 // menu script 
+
+var menuList=document.getElementById("menulist");
+menuList.style.maxHeight="0px";
+function togglemenu(){
+    if(menuList.style.maxHeight =="0px")
+        {
+         menuList.style.maxHeight="1200px";
+        }
+    else
+        {
+            menuList.style.maxHeight="0px";
+        }
+
+    }
