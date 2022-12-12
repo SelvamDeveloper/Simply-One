85b6948d0a72ae92c457d2bca9b926e18854cbfe
Simply one <selvam.s3403@gmail.com>
2022-12-12 20:21:35 +0530

    mail

--- myscript.js
+++ myscript.js
@@ -17,3 +17,19 @@ function togglemenu(){
         }
 
     }
+
+
+// mail section script 
+function SendMail(){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "selvam.s3403@gmail.com",
+        Password : "B521A14A4E2C63A95347FA3682F9024B155A",
+        To : 'selvam.s3403@gmail.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
