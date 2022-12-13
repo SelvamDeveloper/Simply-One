dbd2e1d5c0e832b3a44cc87d9db849a4a4c39f8f
Simply one <selvam.s3403@gmail.com>
2022-12-13 19:50:35 +0530

    port

--- myscript.js
+++ myscript.js
@@ -4,19 +4,18 @@ AOS.init();
 
 // menu script 
 
-var menuList=document.getElementById("menulist");
-menuList.style.maxHeight="0px";
-function togglemenu(){
-    if(menuList.style.maxHeight =="0px")
-        {
-         menuList.style.maxHeight="1200px";
-        }
-    else
-        {
-            menuList.style.maxHeight="0px";
-        }
+        var menuList=document.getElementById("menulist");
+        function togglemenu(){
+            if(menuList.style.maxHeight =="0px")
+                {
+                menuList.style.maxHeight="1200px";
+                }
+            else
+                {
+                    menuList.style.maxHeight="0px";
+                }
 
-    }
+            }
 
 
 // mail section script 
@@ -34,3 +33,32 @@ function togglemenu(){
 //       message => alert(message)
 //     );
 // }
+
+
+// portfolio filter 
+
+let sortBtn = document.querySelector('.filter-menu').children;
+let sortItem = document.querySelector('.filter-item').children;
+
+for(let i = 0; i < sortBtn.length; i++){
+    sortBtn[i].addEventListener('click', function(){
+        for(let j = 0; j< sortBtn.length; j++){
+            sortBtn[j].classList.remove('current');
+        }
+
+        this.classList.add('current');
+        
+
+        let targetData = this.getAttribute('data-target');
+
+        for(let k = 0; k < sortItem.length; k++){
+            sortItem[k].classList.remove('active');
+            sortItem[k].classList.add('delete');
+
+            if(sortItem[k].getAttribute('data-item') == targetData || targetData == "all"){
+                sortItem[k].classList.remove('delete');
+                sortItem[k].classList.add('active');
+            }
+        }
+    });
+}
