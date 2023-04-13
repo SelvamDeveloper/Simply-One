37f60b67fbc5b8a476e58f7c19ce0d597ed96236
Simply one <selvam.s3403@gmail.com>
2023-04-13 23:12:17 +0530

    a

--- myscript.js
+++ myscript.js
@@ -18,21 +18,6 @@ AOS.init();
             }
 
 
-            const burgAnimation = () =>{
-                var burger = document.querySelector('.svgburg')
-                 var path1 = document.querySelector('.path1')
-                 var path2 = document.querySelector('.path2')
-                 var mline = document.querySelector('.mline')
-                 burger.addEventListener('click',() =>{     
-                     path1.classList.toggle('cross');
-                     path2.classList.toggle('cross');
-                     mline.classList.toggle('hide');
-                    }
-                  )
-              
-              }
-             burgAnimation();
-
 
 // cursor movement script 
 
